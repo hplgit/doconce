23f869619468de17184d2f3fe593d32091be0cca
Hans Petter Langtangen <hpl@simula.no>
2013-09-19 09:14:43 +0200

    updates

--- README.sh
+++ README.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Transform README.do.txt to README.md in GitHub flavored Markdown format
 doconce format pandoc README --github_md
+
+# The Highlights section of the README file is also featured on the
+# main web page in doc/web/index.html, so every update of that section
+# in README.do.txt should be propagated manually to the index.html page.
