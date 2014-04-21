eb4bcfb8ab6e694b642c943562b90ae142c069c9
Hans Petter Langtangen <hpl@simula.no>
2014-04-22 00:41:48 +0200

    Fixed bug in non-breaking space char in rst/sphinx.

--- lib/doconce/common.py
+++ lib/doconce/common.py
@@ -852,7 +852,7 @@ INLINE_TAGS = {
     #'non-breaking-space': r'(?<=[$A-Za-z0-9])~(?=[$A-Za-z0-9])',
     # This one allows HTML MathJax formulas and HTML tags to surround the ~
     # (i.e., after substitutions of $...$, color, etc.)
-    'non-breaking-space': r'(?<=[})>$A-Za-z0-9])~(?=[{\\<$A-Za-z0-9])'
+    'non-breaking-space': r'(?<=[})>$A-Za-z0-9_`])~(?=[{\\<$A-Za-z0-9`:])'
 
     }
 
