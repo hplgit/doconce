3999696c1a842d3468fa7c80da5d2446d16d13f6
Hans Petter Langtangen <hpl@simula.no>
2014-06-19 16:57:26 +0200

    updates

--- doc/src/pyembed/src/ex1.py
+++ doc/src/pyembed/src/ex1.py
@@ -1,6 +1,6 @@
 import sympy as sm
 x, y, a = sm.symbols('x y a')
-f = a*x + y*sm.sin(y)
+f = a*x + y**2*sm.sin(y)
 step1 = sm.Integral(f, x, y)
 print step1
 step2 = sm.Integral(sm.Integral(f, x).doit(), y)
