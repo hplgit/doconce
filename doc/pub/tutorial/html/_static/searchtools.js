d7910fba8fe86e614e41d4c1a88b4ab5bc3a0d06
Hans Petter Langtangen <hpl@simula.no>
2014-07-19 16:42:25 +0200

    updates

--- doc/pub/tutorial/html/_static/searchtools.js
+++ doc/pub/tutorial/html/_static/searchtools.js
@@ -301,7 +301,7 @@ var Search = {
   },
 
   query : function(query) {
-    var stopwords = ["and","then","into","it","as","are","in","if","for","no","there","their","was","is","be","to","that","but","they","not","such","with","by","a","on","these","of","will","this","near","the","or","at"];
+    var stopwords = ["a","and","are","as","at","be","but","by","for","if","in","into","is","it","near","no","not","of","on","or","such","that","the","their","then","there","these","they","this","to","was","will","with"];
 
     // Stem the searchterms and add them to the correct list
     var stemmer = new Stemmer();
