198401940116a1607ae2f00c306f622001de27cf
Hans Petter Langtangen <hpl@simula.no>
2015-05-12 15:54:51 +0200

    updates

--- doc/src/ipynb/make.sh
+++ doc/src/ipynb/make.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 doconce format ipynb example
 
-ipynb_figure=md
+ipynb_figure=imgtag
 ipynb_movie=ipynb
 ipynb_admon=hrule
+nbv=3
 
-doconce format ipynb info --no_preprocess --ipynb_figure=${ipynb_figure} ipynb_figure=${ipynb_figure} --ipynb_movie=${ipynb_movie} ipynb_movie=${ipynb_movie} --ipynb_admon=${ipynb_admon} ipynb_admon=${ipynb_admon} --ipynb_version=3 --encoding=utf-8
+doconce format ipynb info --no_preprocess --ipynb_figure=${ipynb_figure} ipynb_figure=${ipynb_figure} --ipynb_movie=${ipynb_movie} ipynb_movie=${ipynb_movie} --ipynb_admon=${ipynb_admon} ipynb_admon=${ipynb_admon} --ipynb_version=$nbv --encoding=utf-8
 # Must fix instructions since doconce performs certain actions for
 # some of the code segments we demonstrate
 doconce subst '" +%matplotlib inline\\n",\n +" +\\n",\n +' '' info.ipynb
