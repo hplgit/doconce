044aaa55a7991da8cc92fc511d30cf6dd914e56a
Hans Petter Langtangen <hpl@simula.no>
2015-05-14 03:47:33 -0700

    updates

--- doc/src/pyembed/make.sh
+++ doc/src/pyembed/make.sh
@@ -1,5 +1,6 @@
 name=pyembed
-doconce format html $name --html_style=bootstrap_bluegray --html_code_style=inherit
+doconce format html $name --html_style=bootstrap_bluegray --html_code_style=inherit --html_box_shadow=True
+
 doconce format pdflatex $name --latex_code_style=pyg
 pdflatex -shell-escape $name
 
