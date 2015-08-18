01c19cfc560194cb091ed9935635a80eef1042d2
Hans Petter Langtangen <hpl@simula.no>
2015-08-18 15:37:01 -0700

    updates

--- doc/src/manual/make.sh
+++ doc/src/manual/make.sh
@@ -41,7 +41,7 @@ publish import refs3.bib <<EOF
 EOF
 
 # doconce html format:
-system doconce format html manual.do.txt --no_mako --html_style=bootswatch_readable --no_abort
+system doconce format html manual.do.txt --no_mako --html_style=bootswatch_readable --no_abort --html_code_style=inherit
 system doconce split_tmp.html manual.html
 
 # Sphinx
