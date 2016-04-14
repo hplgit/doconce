6153c76f9b6e30bc11402ff4866abb3a7a14f37f
Hans Petter Langtangen <hpl@simula.no>
2016-04-14 10:26:07 -0700

    Switched to v1.3.

--- lib/doconce/__init__.py
+++ lib/doconce/__init__.py
@@ -86,7 +86,7 @@ Both directories contain a make.sh file for creating various formats.
 
 '''
 
-__version__ = '1.2'
+__version__ = '1.3'
 version = __version__
 __author__ = 'Hans Petter Langtangen', 'Johannes H. Ring'
 author = __author__
