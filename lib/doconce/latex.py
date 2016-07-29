2c7d8f26d538bb2b8d70eb1ca1816eee4f70ef85
Hans Petter Langtangen <hpl@simula.no>
2016-07-29 10:21:57 -0700

    merge

--- lib/doconce/latex.py
+++ lib/doconce/latex.py
@@ -1641,7 +1641,8 @@ def latex_title(m):
     section_headings = option('latex_section_headings=', 'std')
 
     if latex_style in ("Springer_sv", "Springer_T2", "Springer_T4",
-                       "Springer_lncse", "Springer_lnup", "tufte-book"):
+                       "Springer_lncse", "Springer_lnup", "tufte-book",
+                       "siambook"):
         text += r"""
 \frontmatter
 \setcounter{page}{3}
@@ -1990,7 +1991,15 @@ def latex_abstract(m):
     else:
         if atype == 'preface':
             # book abstract
-            if latex_style not in ('Springer_sv', 'Springer_lnup'):
+            if latex_style == 'siambook':
+                abstract += r"""
+%% --- begin preface ---
+\begin{thepreface}
+%(text)s
+\end{end{preface}
+%% --- end preface ---
+""" % vars()
+            elif latex_style not in ('Springer_sv', 'Springer_lnup'):
                 # Must probably adjust this test for various book formats...
                 abstract += r"""
 %% --- begin preface ---
@@ -2847,7 +2856,7 @@ def define(FILENAME_EXTENSION,
 \vspace{1cm} % after toc
 """
     if latex_style in ('Springer_sv', 'Springer_lnup', 'tufte-book',
-                       'Springer_T2', 'Springer_T4'):
+                       'Springer_T2', 'Springer_T4', 'siambook'):
         toc_part += r"""
 \mainmatter
 """
@@ -3048,6 +3057,11 @@ justified,
         INTRO['latex'] += r"""
 %% Style: SIAM LaTeX2e multimedia
 \documentclass[leqno,%(draft)s]{siamltexmm}
+""" % vars()
+    elif latex_style == 'siambook':
+        INTRO['latex'] += r"""
+%% Style: SIAMGHbook2016 for SIAM books
+\documentclass[%(draft)s]{SIAMGHbook2016}
 """ % vars()
     elif latex_style == 'elsevier':
         INTRO['latex'] += r"""
@@ -3389,6 +3403,16 @@ justified,
         INTRO['latex'] += r"""
 \usepackage[T1]{fontenc}
 %\usepackage[latin1]{inputenc}
+\usepackage{ucs}
+\usepackage[utf8x]{inputenc}
+"""
+    elif latex_style == 'siambook':
+        INTRO['latex'] += r"""
+\usepackage[urw-garamond,sfscaled=false]{mathdesign}
+\usepackage[T1]{fontenc}
+\renewcommand{\sfdefault}{phv}
+\renewcommand{\ttdefault}{cmtt}
+\renewcommand\bfdefault{b}
 \usepackage{ucs}
 \usepackage[utf8x]{inputenc}
 """
