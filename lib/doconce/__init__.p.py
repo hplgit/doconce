9500cd8b3497cea8f7a5358acaa0089048e7b929
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2017-07-04 16:58:52 +0200

    Version 1.4.1: New Mathjax CDN

--- lib/doconce/__init__.p.py
+++ lib/doconce/__init__.p.py
@@ -5,7 +5,7 @@
 '''
 from __future__ import absolute_import
 
-__version__ = '1.4'
+__version__ = '1.4.1'
 version = __version__
 __author__ = 'Hans Petter Langtangen', 'Johannes H. Ring', 'Kristian Gregorius Hustad'
 author = __author__
