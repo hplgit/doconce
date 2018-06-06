d38f374acece0fce1acf7836c7c5757af12b27ad
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2018-06-06 19:16:09 +0200

    Bump version to 1.4.8

--- lib/doconce/__init__.p.py
+++ lib/doconce/__init__.p.py
@@ -5,7 +5,7 @@
 '''
 from __future__ import absolute_import
 
-__version__ = '1.4.7'
+__version__ = '1.4.8'
 version = __version__
 __author__ = 'Hans Petter Langtangen', 'Johannes H. Ring', 'Kristian Gregorius Hustad'
 author = __author__
